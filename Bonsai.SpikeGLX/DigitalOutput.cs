083a84e1ce69c5ee872a32a41a3847453cdf2ce8
Jacob White <jacobmw@mit.edu>
2023-11-26 13:41:36 -0500

    Add Main files via upload

---
+++ Bonsai.SpikeGLX/DigitalOutput.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+
+namespace Bonsai.SpikeGLX
+{
+    // <summary>
+    /// Represents an operator that controls a digital output.
+    /// </summary>
+    [Combinator]
+    [Description("Controls a digital output.")]
+    [WorkflowElementCategory(ElementCategory.Sink)]
+    public class DigitalOutput
+    {
+        /// <summary>
+        /// Gets or sets the IP address of the SpikeGLX command server
+        /// </summary>
+        [Description("IP Address of the SpikeGLX command server.")]
+        public string Host { get; set; } = "localhost";
+
+        /// <summary>
+        /// Gets or sets the port of the SpikeGLX command server.
+        /// </summary>
+        [Description("Port of the SpikeGLX command server.")]
+        public int Port { get; set; } = 4142;
+
+        /// <summary>
+        /// Gets or sets the channels to output the digital signal to.
+        /// Strings have format "Dev6/port0/line2,Dev6/port0/line5\".
+        /// </summary>
+        [Description("Channels to output the digital signal to. " +
+            "Strings have format \"Dev6/port0/line2,Dev6/port0/line5\"")]
+        public string Channels { get; set; }
+
+        /// <summary>
+        /// Set a digital output.
+        /// </summary>
+        /// <param name="source">
+        /// A sequence of <see cref="int"/>s, each representing a high (1) or low (0) digital state.
+        /// </param>
+        /// <returns>
+        /// An observable sequence that is identical to the <paramref name="source"/> sequence
+        /// but where there is an additional side effect of setting a digital output. 
+        /// </returns>
+        public IObservable<int> Process(IObservable<int> source)
+        {
+            return Observable.Using<int, SpikeGLX>(() => new SpikeGLX(Host, Port),
+                connection => source.Do(input => connection.SetDigitalOut(input, Channels)));
+        }
+    }
+}
---
+++ Bonsai.SpikeGLX/SpikeGLXDataStream.cs
@@ -0,0 +1,123 @@
+﻿using OpenCV.Net;
+
+namespace Bonsai.SpikeGLX
+{
+    /// <summary>
+    /// Represents an instance of a single SpikeGLX data stream being accessed from Bonsai.
+    /// </summary>
+    public class SpikeGLXDataStream : SpikeGLX
+    {
+        protected readonly int JS;              // Stream type (0: NIDAQ, 1: Onebox, 2: IMEC Probe).  
+        protected readonly int IP;              // Substream (0 for NIDAQ, probe number for IMEC Probe).
+        protected readonly int[] Channels;      // Channels to fetch data from.
+        protected readonly double[] Mults;      // Constants to convert raw data to voltages for each channel.
+        protected readonly double SampleRate;   // Channel sample rate.
+
+        /// <summary>
+        /// Initialise an instance of a single SpikeGLX data stream communicating with Bonsai.
+        /// </summary>
+        /// <param name="host">IP address of the SpikeGLX command server.</param>
+        /// <param name="port">Port of the SpikeGLX command server.</param>
+        /// <param name="js">Stream type (0: NIDAQ, 1: Onebox, 2: IMEC Probe).</param>
+        /// <param name="ip">Substream (0 for NIDAQ, probe number for IMEC Probe).</param>
+        /// <param name="channels">Channels to fetch data from.</param>
+        /// <exception cref="SpikeGLXException"></exception>
+        public SpikeGLXDataStream(string host, int port, int js, int ip, int[] channels) : base(host, port)
+        {
+            // Set stream parameters
+            JS = js;
+            IP = ip;
+            Channels = channels;
+
+            // Get stream sample rate.
+            SampleRate = GetStreamSampleRate(JS, IP);
+
+            // Get multiples for converting raw data to voltages.
+            Mults = new double[channels.Length];
+            for (int i = 0; i < channels.Length; i++)
+            {
+                Mults[i] = GetStreamI16ToVolts(JS, IP, channels[i]);
+            }
+
+        }
+
+        /// <summary>
+        /// Get the number of samples in the data stream.
+        /// </summary>
+        /// <returns>The number of samples in the data stream.</returns>
+        public ulong GetStreamSampleCount()
+        {
+            return GetStreamSampleCount(JS, IP);
+        }
+
+        /// <summary>
+        /// Get the sample rate of the given data stream.
+        /// </summary>
+        /// <returns>The stream sample rate.</returns>
+        public double GetStreamSampleRate()
+        {
+            return SampleRate;
+        }
+
+        /// <summary>
+        /// Convert a matrix of raw data from the data stream into a matrix
+        /// of voltages, in mV.
+        /// </summary>
+        /// <param name="data">The data matrix to convert.</param>
+        /// <returns>The converted data matrix.</returns>
+        private Mat ConvertToVoltage(Mat data)
+        {
+            // Initialise the new data matrix
+            Mat dataVolts = Mat.Zeros(data.Rows, data.Cols, Depth.F64, 1);
+
+            // Mulitply each channel (row) by its associated multiple
+            for (int row = 0; row < data.Rows; row++)
+                for (int col = 0; col < data.Cols; col++)
+                {
+                    dataVolts.SetReal(row, col, (double)data.GetReal(row, col) * Mults[row]);
+                }
+
+            // Dispose of the original data matrix
+            data.Close();
+
+            return dataVolts;
+        }
+
+        /// <summary>
+        /// Fetch data from the SpikeGLX data stream.
+        /// </summary>
+        /// <param name="data">
+        /// Matrix of fetched data. Each row is a channel, each column is a sample.
+        /// </param>
+        /// <param name="startSample">Sample count to fetch from.</param>
+        /// <param name="maxSamples">Maximum number of samples to fetch.</param>
+        /// <param name="downsample">Factor by which to downsample fetched data.</param>
+        /// <param name="mV">Convert the data to a voltage, in mV.</param>
+        /// <returns>Sample count at the beginning of the fetched data.</returns>
+        /// <exception cref="SpikeGLXException"></exception>
+        public ulong Fetch(out Mat data, ulong startSample, int maxSamples, int downsample, bool mV)
+        {
+            ulong headCount = Fetch(out data, JS, IP, startSample, maxSamples, Channels, downsample);
+            if (mV) data = ConvertToVoltage(data);
+            return headCount;
+        }
+
+        /// <summary>
+        /// Fetch the latest data from the SpikeGLX data stream.
+        /// </summary>
+        /// <param name="data">
+        /// Matrix of fetched data. Each row is a channel, each column is a sample.
+        /// </param>
+        /// <param name="maxSamples">Maximum number of samples to fetch.</param>
+        /// <param name="downsample">Factor by which to downsample fetched data.</param>
+        /// <param name="mV">Convert the data to a voltage, in mV.</param>
+        /// <returns>Sample count at the beginning of the fetched data.</returns>
+        /// <exception cref="SpikeGLXException"></exception>
+        public ulong FetchLatest(out Mat data, int maxSamples, int downsample, bool mV)
+        {
+            ulong headCount = FetchLatest(out data, JS, IP, maxSamples, Channels, downsample);
+            if (mV) data = ConvertToVoltage(data);
+            return headCount;
+        }
+    }
+}
