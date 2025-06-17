9258b26d86cbbf21e079801bcbe8d945dde8668e
David Maas <d.maas+git@neurogears.org>
2025-06-17 10:52:39 -0500

    Conformance

---
+++ docs/template/public/main.js
@@ -0,0 +1,1 @@
+import WorkflowContainer from "./workflow.js"export default {    defaultTheme: 'light',    iconLinks: [{        icon: 'github',        href: 'https://github.com/bonsai-rx/spikeglx',        title: 'GitHub'    }],    start: () => {        WorkflowContainer.init();    }}
